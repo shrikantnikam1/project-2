85ed67330097e3574e60d90eaba412100cf31006
Shrikant Nikam <smnikam007@gmail.com>
2026-02-23 19:16:37 +0530

    Removed node_modules

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/products", (req, res) => {
+  res.json([
+    { id: 1, name: "Red Painting", price: 2000 },
+    { id: 2, name: "Blue Sculpture", price: 3500 },
+    { id: 3, name: "Green Vase", price: 1500 },
+    { id: 4, name: "Yellow Tapestry", price: 2500 },
+    { id: 5, name: "Purple Ceramic", price: 1800 }
+    { id: 6, name: "Orange Print", price: 1200 }
+  ]);
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
